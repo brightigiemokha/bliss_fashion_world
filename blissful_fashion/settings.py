57c173c5f009903e322aa45c09288f258185bd01
brightigiemokha <brightigiemokha@yahoo.com>
2024-02-01 18:35:21 +0000

    still trying to get static files

--- blissful_fashion/settings.py
+++ blissful_fashion/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['blissful-fashion-0cb729cb8dac.herokuapp.com/', '8000-brightigiem-blissfashio-642wjtc6xfy.ws-eu107.gitpod.io']
-CSRF_TRUSTED_ORIGINS = ['https://blissful-fashion-0cb729cb8dac.herokuapp.com/', ]
+CSRF_TRUSTED_ORIGINS = ['https://blissful-fashion-0cb729cb8dac.herokuapp.com/',]
 
 
 # Application definition
