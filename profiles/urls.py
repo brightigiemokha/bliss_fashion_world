4b7aa15515e4f4d3d299981665253c55d6fef1cb
brightigiemokha <brightigiemokha@yahoo.com>
2024-02-06 23:23:58 +0000

    fixed contact view page

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
+    path('contact_view/', views.contact_view, name='contact_view'),
 ]
