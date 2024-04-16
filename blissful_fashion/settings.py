10eb76dcd3d6ea2fc7ba6231fc0ce3182eef2daf
brightigiemokha <brightigiemokha@yahoo.com>
2024-04-16 16:46:37 +0000

    changed site id to 14

--- blissful_fashion/settings.py
+++ blissful_fashion/settings.py
@@ -146,7 +146,7 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-SITE_ID = 12
+SITE_ID = 14
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
